cd355dfcd2af84f10741c3c340df41569b85a7bf
Leonardo <leonardolima2008@gmail.com>
2020-04-09 16:56:45 -0300

    projeto

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -1,42 +1,55 @@
+function horaDoDia(evento) {
+  const select = document.getElementById("hora-dia")
+  const option = select.children[select.selectedIndex]
+  evento = option.textContent
+
+  return evento
+}
+
+function diaSemana(evento) {
+  const select = document.getElementById("dia-semana")
+  const option = select.children[select.selectedIndex]
+  evento = option.value
+
+  return evento
+}
+
 function cadastrarTarefa() {
   const tarefa = document.getElementById("input-texto")
-  const diaDaSemana = document.getElementById("diaDaSemana")
-  const escolhaDiaDaSemana = diaDaSemana.options[diaDaSemana.selectedIndex].value
-  const tarefaSegunda = document.getElementById("segunda")
-  const tarefaTerca = document.getElementById("terca")
-  const tarefaQuarta = document.getElementById("quarta")
-  const tarefaQuinta = document.getElementById("quinta")
-  const tarefaSexta = document.getElementById("sexta")
-  const tarefaSabado = document.getElementById("sabado")
-  const tarefaDomingo = document.getElementById("domingo")
+  let i = document.getElementById(diaSemana())
+  let horaAgendada = horaDoDia()
 
   if (tarefa.value === "" || tarefa.value >= 0 || tarefa.value <= 0) {
     alert("Necessário informar um nome para Tarefa  e/ou a Tarefa não pode ter apenas números")
     return tarefa.value = ""
-  }
-  if (escolhaDiaDaSemana === "segunda") {
-    tarefaSegunda.innerHTML += "<li>" + tarefa.value + "</li>"
 
-  } else if (escolhaDiaDaSemana === "terca") {
-    tarefaTerca.innerHTML += "<li>" + tarefa.value + "</li>"
-
-  } else if (escolhaDiaDaSemana === "quarta") {
-    tarefaQuarta.innerHTML += "<li>" + tarefa.value + "</li>"
-
-  } else if (escolhaDiaDaSemana === "quinta") {
-    tarefaQuinta.innerHTML += "<li>" + tarefa.value + "</li>"
-
-  } else if (escolhaDiaDaSemana === "sexta") {
-    tarefaSexta.innerHTML += "<li>" + tarefa.value + "</li>"
-
-  } else if (escolhaDiaDaSemana === "sabado") {
-    tarefaSabado.innerHTML += "<li>" + tarefa.value + "</li>"
 
   } else {
-    tarefaDomingo.innerHTML += "<li>" + tarefa.value + "</li>"
-
+    i.innerHTML += "<li >" + horaAgendada + " " + tarefa.value + "</li>"
   }
   tarefa.value = ""
+
 }
 
+function apertouEnter(evento) {
+  if (evento.key === "Enter") {
+    cadastrarTarefa()
+  }
+}
 
+function deletarTarefa() {
+  for (let i = 0; i < 6; i++) {
+    i = document.getElementById(diaSemana()).innerHTML = ""
+
+    console.log(i)
+    // let tarefaSegunda = document.getElementById("segunda").innerHTML = ""
+    // let tarefaTerca = document.getElementById("terca").innerHTML = ""
+    // let tarefaQuarta = document.getElementById("quarta").innerHTML = ""
+    // let tarefaQuinta = document.getElementById("quinta").innerHTML = ""
+    // let tarefaSexta = document.getElementById("sexta").innerHTML = ""
+    // let tarefaSabado = document.getElementById("sabado").innerHTML = ""
+    // let tarefaDomingo = document.getElementById("domingo").innerHTML = ""
+
+  }
+
+}
