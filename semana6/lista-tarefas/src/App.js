087f94a155bdb864b19eb3861eee3f4c4d14abf9
Leonardo <leonardolima2008@gmail.com>
2020-04-23 05:11:48 -0300

    exercicio atualizado ainda nao esta completo

--- semana6/lista-tarefas/src/App.js
+++ semana6/lista-tarefas/src/App.js
@@ -1,109 +1,168 @@
-import React from 'react'
-import styled from 'styled-components'
-import './styles.css'
+import React from "react";
+import "./styles.css";
+import ListaTarefas from "./Componentes/ListaTarefas";
+import styled from "styled-components";
 
-const TarefaList = styled.ul`
-  padding: 0;
-  width: 200px;
-`
+const Button = styled.button`
+  border-style: none;
+  margin: 5px;
+  height: 30px;
+  width: 90px;
+  background-color: #000;
+  color: #fff;
+  text-transform: uppercase;
+  cursor: pointer;
+`;
 
-const Tarefa = styled.li`
-  text-align: left;
-  text-decoration: ${({ completa }) => (completa ? 'line-through' : 'none')};
-`
-
-const InputsContainer = styled.div`
-  display: grid;
-  grid-auto-flow: column;
-  gap: 10px;
-`
+const Input = styled.input`
+  height: 24px;
+  width: 250px;
+`;
+const Menu = styled.div`
+  text-align: center;
+  margin-bottom: 20px;
+`;
+const Filtros = styled.div`
+  text-transform: uppercase;
+  text-align: right;
+  margin-right: 27px;
+`;
 
 class App extends React.Component {
   state = {
-    tarefas: [{
-      id: Date.now(),
-      texto: '',
-      completa: false
-    }],
-    inputValue: '',
-    filter: ""
-  }
+    tarefas: [
+      {
+        id: 0,
+        texto: "teste 1",
+        completa: false
+      }
+    ],
+    inputTexto: "",
+    filtro: "",
+    relCompletas: ""
+  };
 
-  onChangeInput = (event) => {
-    this.setState({ inputValue: event.target.value })
-  }
+  onChangeTarefa = event => {
+    this.setState({ inputTexto: event.target.value });
+  };
 
-  criaTarefa = () => {
+  onKeyDown = event => {
+    if (event.key === "Enter") {
+      this.criarTarefa();
+    }
+  };
+
+  criarTarefa = () => {
     const novaTarefa = {
       id: Date.now(),
-      texto: this.state.inputValue,
+      texto: this.state.inputTexto,
       completa: false
-    }
-    const novaListaTarefas = [novaTarefa, ...this.state.tarefas]
+    };
 
-    this.setState({ novaTarefa: novaListaTarefas })
-    console.log(novaTarefa)
+    const novaListaTarefa = [novaTarefa, ...this.state.tarefas];
 
-  }
+    this.setState({ tarefas: novaListaTarefa });
+  };
+
+  alterarTarefa = tarefaId => {
+    const novaTarefa = this.state.tarefas.map(tarefa => {
+      if (tarefaId === tarefa.id) {
+        const mudarTarefa = {
+          ...tarefa,
+          completa: !tarefa.completa
+        };
+        return mudarTarefa;
+      } else {
+        return tarefa;
+      }
+    });
+
+    this.setState({ tarefas: novaTarefa });
+  };
+
+
 
   selectTarefa = (id) => {
 
-    id = this.tarefas.id
-
   }
 
-  onChangeFilter = (event) => {
-    this.setState({filtro: event.target.value})
-  }
+  onChangeFiltro = event => {
+    this.setState({ filtro: event.target.value });
+  };
 
   render() {
-
-    const listaFiltrada = this.state.tarefas
-      .filter(tarefa => {
-        switch (this.state.filter) {
-          case 'pendentes':
-            return !tarefa.completa
-          case 'completas':
-            return tarefa.completa
-          default:
-            return true
-        }
-      })
+    const listaFiltrada = this.state.tarefas.filter(tarefa => {
+      switch (this.state.filtro) {
+        case 'pendentes':
+          return !tarefa.completa
+        case 'completas':
+          return tarefa.completa
+        default:
+          return false
+      }
+    })
 
     return (
       <div className="App">
-        <h1>Lista de tarefas</h1>
-        <InputsContainer>
-          <input
-            value={this.state.inputValue}
-            onChange={this.onChangeInput} />
-          <button onClick={this.criaTarefa}>Adicionar</button>
-        </InputsContainer>
-        <br />
+        <div>
+          <h1>Lista Tarefas</h1>
+        </div>
+        <Menu>
+          <Input
+            type="text"
+            onChange={this.onChangeTarefa}
+            onKeyDown={this.onKeyDown}
+            value={this.state.inputTexto}
+            placeholder={"Adicionar Tarefa"}
+          />
 
-        <InputsContainer>
-          <label>Filtro</label>
-          <select value={this.state.filter} onChange={this.onChangeFilter}>
-            <option value="">Nenhum</option>
-            <option value="pendentes">Pendentes</option>
-            <option value="completas">Completas</option>
+          <Button onClick={this.criarTarefa}>Adicionar</Button>
+          <Button onClick={this.criarTarefa}>Apagar</Button>
+        </Menu>
+        <Filtros>
+          <span>filtrar</span>
+          <select value={this.state.filtro} onChange={this.onChangeFiltro}>
+            <option id="1" value="nenhum">
+              nenhum
+            </option>
+            <option id="2" value="completas">
+              completas
+            </option>
+            <option id="3" value="pendentes">
+              pendentes
+            </option>
           </select>
-        </InputsContainer>
-        <TarefaList>
-          {listaFiltrada.map(tarefa => {
+        </Filtros>
+        <div>
+          <h5>Lista</h5>
+          {this.state.tarefas.map(tarefa => {
             return (
-              <Tarefa
+              <ListaTarefas
+                key={tarefa.id}
+                tarefa={tarefa}
                 completa={tarefa.completa}
-                onClick={() => this.selectTarefa(tarefa.id)}
-              >
-                {tarefa.texto}
-              </Tarefa>
-            )
+                alterarTarefa={this.alterarTarefa}
+              />
+            );
           })}
-        </TarefaList>
+
+        </div>
+        <div><h4>Relatórios</h4></div>
+        {listaFiltrada.map(tarefa => {
+          return (<div
+            completa={tarefa.completa}
+            onclick={() => this.selectTarefa(tarefa.id)}
+          >
+
+
+            {tarefa.texto}
+          </div>
+          )
+        })}
+
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
