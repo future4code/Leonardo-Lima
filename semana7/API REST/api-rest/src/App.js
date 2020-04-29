37df28cb75b0c9e0bf338826b246878fe5ecf164
Leonardo <leonardolima2008@gmail.com>
2020-04-29 15:55:17 -0300

    atualizando dados do exercicio

--- semana7/API REST/api-rest/src/App.js
+++ semana7/API REST/api-rest/src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import  Usuarios  from  "./Components/Usuarios";
+import Cadastro from "./Components/Cadastro";
+import styled from "styled-components"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+  state = {
+    paginaAtual: "CADASTRO"
+  };
+
+  componentDidMount() {
+
+  }
+
+  mudarPagina = () => {
+    const pagina = this.state.paginaAtual === "CADASTRO" ? "CRIAR" : "CADASTRO"
+
+    this.setState({ paginaAtual: pagina })
+  }
+
+   render() {
+
+
+    return (
+      <div className="App">
+        <div className="UsuariosLista">
+          {this.state.paginaAtual === 'CADASTRO' ? <Cadastro /> : <Usuarios />}
+          <button onClick={this.mudarPagina}>{this.state.paginaAtual === 'CADASTRO' ? 'Exibir Lista de Usuários' : 'Voltar para Cadastro'}</button>
+        </div>
+
+        </div>
+
+    );
+  }
 }
-
 export default App;
