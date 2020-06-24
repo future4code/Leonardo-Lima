b093257bf8c985f83627188cf45553b9a0b891cc
Leonardo Lima <leonardolima2008@gmail.com>
2020-06-24 16:48:02 -0300

    exercicios aula 46

---
+++ semana14/aula46/src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var moment = require("moment");
+var today = moment();
+console.log(today.format('DD [de] MMM [de] YYYY'));
---
+++ semana14/aula46/src/index.ts
@@ -0,0 +1,6 @@
+import * as moment from 'moment'
+
+const today = moment()
+
+
+console.log(today.format('DD [de] MMM [de] YYYY'))
